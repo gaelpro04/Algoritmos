8c729740099253830a5519688537957ee2e5ff41
Gael <gael.lopez22@uabc.edu.mx>
2025-01-28 21:48:17 -0700

    Primer avance

---
+++ Instrumento.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Instrumento {
+
+    private String nombre;
+    private String tipo;
+    private ArrayList<String> autores;
+    private boolean evaluacion;
+    private int clave;
+    private static int claveActual = 0;
+    private String referencia;
+
+    public Instrumento(String nombre, String tipo, boolean evaluacion, String referencia)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.evaluacion = evaluacion;
+        this.referencia = referencia;
+
+        claveActual++;
+        clave = claveActual;
+
+        System.out.println("Ingresa la cantidad de autores");
+        int cantidadAutores = sc.nextInt();
+        autores = new ArrayList<>(cantidadAutores);
+
+        for (int i = 0; i < cantidadAutores; i++) {
+            System.out.println("Ingresa el autor numero " + (i+1));
+            autores.add(sc.next());
+        }
+    }
+
+    public String getTipo()
+    {
+        return tipo;
+    }
+
+    public void setTipo(String tipo)
+    {
+        this.tipo = tipo;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<String> getAutores()
+    {
+        return autores;
+    }
+
+    public void setAutores(ArrayList<String> autores)
+    {
+        this.autores = autores;
+    }
+
+    public boolean isEvaluacion()
+    {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(boolean evaluacion)
+    {
+        this.evaluacion = evaluacion;
+    }
+
+    public int getClave()
+    {
+        return clave;
+    }
+
+    public String getReferencia()
+    {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia)
+    {
+        this.referencia = referencia;
+    }
+}
