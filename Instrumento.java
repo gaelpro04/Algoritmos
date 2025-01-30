282cef96ac48c45324d21294b2073dc839d8e752
Gael <gael.lopez22@uabc.edu.mx>
2025-01-30 13:24:05 -0700

    Segundo avance

--- Instrumento.java
+++ Instrumento.java
@@ -11,8 +11,7 @@ public class Instrumento {
     private static int claveActual = 0;
     private String referencia;
 
-    public Instrumento(String nombre, String tipo, boolean evaluacion, String referencia)
-    {
+    public Instrumento(String nombre, String tipo, boolean evaluacion, String referencia) {
         Scanner sc = new Scanner(System.in);
 
         this.nombre = nombre;
@@ -28,63 +27,63 @@ public class Instrumento {
         autores = new ArrayList<>(cantidadAutores);
 
         for (int i = 0; i < cantidadAutores; i++) {
-            System.out.println("Ingresa el autor numero " + (i+1));
+            System.out.println("Ingresa el autor numero " + (i + 1));
             autores.add(sc.next());
         }
     }
 
-    public String getTipo()
-    {
+    public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo)
-    {
+    public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
-    public String getNombre()
-    {
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre)
-    {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public ArrayList<String> getAutores()
-    {
+    public ArrayList<String> getAutores() {
         return autores;
     }
 
-    public void setAutores(ArrayList<String> autores)
-    {
+    public void setAutores(ArrayList<String> autores) {
         this.autores = autores;
     }
 
-    public boolean isEvaluacion()
-    {
+    public boolean isEvaluacion() {
         return evaluacion;
     }
 
-    public void setEvaluacion(boolean evaluacion)
-    {
+    public void setEvaluacion(boolean evaluacion) {
         this.evaluacion = evaluacion;
     }
 
-    public int getClave()
-    {
+    public int getClave() {
         return clave;
     }
 
-    public String getReferencia()
-    {
+    public String getReferencia() {
         return referencia;
     }
 
-    public void setReferencia(String referencia)
-    {
+    public void setReferencia(String referencia) {
         this.referencia = referencia;
+    }
+
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+        for (String nombres : autores) {
+            builder.append(nombres).append(" ");
+        }
+        String nombres = builder.toString();
+
+        return "Nombre: " + nombre + "Tipo: " + nombres + "Evaluacion: " + (evaluacion ? "certificado" : "no certificado")  + "Clave: " + clave + "Referencia: " + referencia;
     }
 }
