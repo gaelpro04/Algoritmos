9ac54acd5cdca65803eb8428c3fe358c375ece31
Gael <gael.lopez22@uabc.edu.mx>
2025-02-19 15:22:50 -0700

    onichan

--- Rec/src/Recursivo.java
+++ Rec/src/Recursivo.java
@@ -51,4 +51,98 @@ public class Recursivo {
         }
         return primoRecursivo(n,division+1);
     }
+
+    /**
+     * Método recursivo para saber si un valor se encuentra en un arreglo
+     * @param x
+     * @param arreglo
+     * @param cont
+     * @return
+     */
+    public int verificarValor(int x, int[] arreglo, int cont)
+    {
+        if (arreglo.length == cont) {
+            return -1;
+        }
+
+        if (arreglo[cont] == x) {
+            return cont;
+        }
+
+        return verificarValor(x,arreglo,cont+1);
+    }
+
+    /**
+     * Método recursivo que entrega el valor más grande de un arreglo
+     * @param arreglo
+     * @param cont
+     * @param indiceMasAlto
+     * @return
+     */
+    public int valorMasGrande(int[]arreglo, int cont, int indiceMasAlto)
+    {
+        int valorAlto = arreglo[indiceMasAlto];
+        if (arreglo.length == cont) {
+            return valorAlto;
+        }
+
+        if (valorAlto < arreglo[cont]) {
+            indiceMasAlto = cont;
+        }
+
+        return valorMasGrande(arreglo, cont+1,indiceMasAlto);
+    }
+
+    /**
+     * Método recursivo que cuenta las orejas de cada conejo
+     * @param cantidadConejos
+     * @param cont
+     * @return
+     */
+    public int conejitos(int cantidadConejos, int cont)
+    {
+        int valor = cont * 2;
+        if (cont == cantidadConejos) {
+            return valor;
+        }
+        return conejitos(cantidadConejos, cont+1);
+    }
+
+    /**
+     * Método recursivo que indica si c1 esta inmediatamente previo del c2
+     * @param cad
+     * @param c1
+     * @param c2
+     * @param cont
+     * @return
+     */
+    public boolean indicarCaracterAnterior(String cad, char c1, char c2, int cont)
+    {
+        if (cont == cad.length() - 1) {
+            return false;
+        }
+        if (cad.charAt(cont) == c1 && cad.charAt(cont+1) == c2) {
+            return true;
+        }
+        return indicarCaracterAnterior(cad,c1,c2,cont+1);
+    }
+
+    /**
+     * Método que suma los numeros que se encuentra en una cadena
+     * @param cad
+     * @param acumulador
+     * @param cont
+     * @return
+     */
+    public int sumaNumeros(String cad, int acumulador, int cont)
+    {
+        if (cad.length() == cont) {
+            return acumulador;
+        }
+
+        if (Character.isDigit(cad.charAt(cont))) {
+            acumulador = acumulador + Character.getNumericValue(cad.charAt(cont));
+        }
+        return sumaNumeros(cad,acumulador,cont+1);
+    }
 }
