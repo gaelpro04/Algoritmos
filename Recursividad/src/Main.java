79f658ba922f3085cd40987f1d56998cd9810cd9
Gael <gael.lopez22@uabc.edu.mx>
2025-02-19 00:21:53 -0700

    onichan

--- Recursividad/src/Main.java
+++ Recursividad/src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        Recursividad1 ajasi = new Recursividad1();
-
-
-        String s = String.valueOf(ajasi.serieN0(5));
-        System.out.println(s);
-        s = ajasi.serie0N(5);
-        System.out.println(s);
-        System.out.println(ajasi.fibonacci(7));
-        System.out.println(ajasi.esPrimo(6));
     }
 }
