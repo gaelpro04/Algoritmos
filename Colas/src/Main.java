0f78c1a97cab0e87036b3ca7c8cc24228d874323
Gael <gael.lopez22@uabc.edu.mx>
2025-02-26 15:51:19 -0700

    when

---
+++ Colas/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        ColaSimple<String> hola = new ColaSimple<>();
+        ColaCircular<String> ajasi = new ColaCircular();
+
+        hola.insertar();
+    }
+}
