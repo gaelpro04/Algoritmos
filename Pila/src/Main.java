2f497af81aefd276e7bd068c991429e53069fe07
Gael <gael.lopez22@uabc.edu.mx>
2025-02-26 17:17:08 -0700

    final

--- Pila/src/Main.java
+++ Pila/src/Main.java
@@ -1,20 +1,14 @@
-public class Main {
+import java.util.Arrays;
+
+public class Main extends Test {
     public static void main(String[] args) {
 
-        Test ajasi = new Test();
-        String simon = "puto";
-        System.out.println(simon);
-        System.out.println(ajasi.invertirCadena(simon));
-
-        String mijadondevan = "2-{32+2(2/2)}";
-        System.out.println(mijadondevan);
-        System.out.println(ajasi.revisarSintaxis(mijadondevan));
-
-        int[] si = {3,4,5,1,9};
-        Pila<Integer> when = ajasi.ordenadoNumeros(si);
-
-        for (int i : si) {
-            System.out.println(when.pop());
+        Test test = new Test();
+        int[] arregloNumeros = {1,2,3,3,6,1};
+        System.out.println(Arrays.toString(arregloNumeros));
+        Pila<Integer> numerosOrdenados = test.ordenadoNumeros(arregloNumeros);
+        for (int i = 0; i < arregloNumeros.length; ++i) {
+            System.out.print(numerosOrdenados.pop() + " ");
         }
     }
 }
