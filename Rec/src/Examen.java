724601d232bac327d1257fba21b289dbcf73e529
Gael <gael.lopez22@uabc.edu.mx>
2025-03-10 20:16:09 -0700

    when

--- Rec/src/Examen.java
+++ Rec/src/Examen.java
@@ -16,4 +16,74 @@ public class Examen {
         }
         return builder.append(n + " " + serieN0(n-1)).toString();
     }
+
+    private boolean esPrimo(int numero, int division) {
+
+        if (numero == 0 || numero == 1) {
+            return false;
+        } else if (numero == 2 || numero == 3) {
+            return true;
+        }
+
+        if (division > numero / 2) {
+            return true;
+        }
+
+        if (numero % division == 0) {
+            return false;
+        }
+
+        return esPrimo(numero, division+1);
+    }
+
+    public boolean esPrimoString(int numero) {
+        return esPrimo(numero, 2);
+    }
+
+    private int fibonacciNumero(int n)
+    {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fibonacciNumero(n-1) + fibonacciNumero(n-2);
+    }
+
+    public String fibonacciString(int n)
+    {
+        StringBuilder fibo = new StringBuilder();
+        if (n == 0) {
+            return fibo.append("0").toString();
+        }
+        return fibo.append(fibonacciString(n-1) + " " +fibonacciNumero(n)).toString();
+    }
+
+    public int sumaNumeros(int n)
+    {
+        if (n==0) {
+            return n;
+        }
+        return n + sumaNumeros(n-1);
+    }
+
+    public int contarCaracteres(String s, char c, int cont, int acum)
+    {
+        if (cont == s.length()) {
+            return acum;
+        }
+        if (s.charAt(cont) == c) {
+            ++acum;
+        }
+
+        return contarCaracteres(s,c,cont+1,acum);
+    }
+
+    public int contarDigitos(int n, int acumulador)
+    {
+        if (n == 0) {
+            return acumulador;
+        }
+        n = n / 10;
+        return contarDigitos(n,acumulador+1);
+    }
 }
--- Rec/src/Main.java
+++ Rec/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
 
         System.out.println(examen.factorial(10));
         System.out.println(examen.serieN0(3));
+
+        System.out.println(examen.esPrimoString(5));
+        System.out.println(examen.fibonacciString(6));
+        System.out.println(examen.sumaNumeros(5));
+        System.out.println(examen.contarCaracteres("como estas", 'o',0,0));
+        System.out.println(examen.contarDigitos(3450,0));
     }
 }
